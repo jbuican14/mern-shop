e4dbec3fc1df591c8eb01b5ddcce4a466183edfe
Bew <Bew@sysops-MBP.broadband>
2021-02-01 13:43:30 +0000

    Adding model mongoose for our database

---
+++ backend/models/product.model.js
@@ -0,0 +1,50 @@
+import mongoose from 'mongoose';
+
+const reviewSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    rating: { type: Number, required: true },
+    comment: { type: String, required: true },
+  },
+  { timestamps: true }
+);
+
+const productSchema = mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: true,
+    ref: 'User',
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+  },
+  reviews: [reviewSchema],
+  rating: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  numReviews: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  price: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+  countInStock: {
+    type: Number,
+    required: true,
+    default: 0,
+  },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+export default Product;
