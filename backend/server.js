691902e1aec256b750cd56c4ff7b842d62f34fcd
Bew <Bew@sysops-MBP.broadband>
2021-02-01 14:57:43 +0000

    Fetching products data from the database [in our own routes/products.routes.js]

--- backend/server.js
+++ backend/server.js
@@ -2,25 +2,20 @@ import dotenv from 'dotenv';
 import color from 'color';
 import express from 'express';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+
+import productRoutes from './routes/product.routes.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
 app.get('/', (req, res) => {
   res.send('API is running.... !');
 });
 
-app.get('/api/products', (req, res) => {
-  res.json(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-  const product = products.find((item) => item._id === req.params.id);
-  res.json(product);
-});
+app.use('/api/products', productRoutes);
 
 // Usage for dotenv package
 const PORT = process.env.PORT || 5000;
