65de960368333086f485a2299af7092dd88fdf4e
Bew <Bew@sysops-MBP.broadband>
2021-02-05 17:55:21 +0000

    Adding user in the backend for registering a new user, fetch user and utils folder for handling jwt token

--- backend/server.js
+++ backend/server.js
@@ -5,24 +5,27 @@ import connectDB from './config/db.js';
 
 import { notFound, errorHandler } from './middleware/error.middleware.js';
 import productRoutes from './routes/product.routes.js';
+import userRoutes from './routes/users.routes.js';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+app.use(express.json()); // allow to accept json in the body
 
 app.get('/', (req, res) => {
   res.send('API is running.... !');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
 // Usage for dotenv package
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(
   PORT,
